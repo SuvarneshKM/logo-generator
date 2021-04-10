57d5d51c4263e686f7ec584af685858ed2301978
Suvarnesh K M <51795492+SuvarneshKM@users.noreply.github.com>
2021-04-11 01:13:29 +0530

    Dark and Light

--- scripts/main.js
+++ scripts/main.js
@@ -9,39 +9,39 @@ function reverseString(str) {
     return str.split("").reverse().join("");
 }
 
-document.getElementById('toggleColor').addEventListener('click', function (evt) {
+document.getElementById('toggleColor').addEventListener('click', function(evt) {
 
-    var cardTemplate = document.querySelector(".card")
+        var cardTemplate = document.querySelector(".card")
 
-    // Check current state
-    if (evt.target.innerHTML === "Dark Theme") {
-        cardTemplate.style.backgroundColor = "#191919";
-        cardTemplate.firstElementChild.style.color = "white";
-        evt.target.innerHTML = "Light Theme";
-        document.getElementById("toggleColor").classList.remove('btn-dark');
-        document.getElementById("toggleColor").classList.add('btn-light');
-        svgLogo.classList.add("is-hidden");
-        svgLogo1.classList.remove("is-hidden");
+        // Check current state
+        if (evt.target.innerHTML === "Dark Theme") {
+            cardTemplate.style.backgroundColor = "#191919";
+            cardTemplate.firstElementChild.style.color = "white";
+            evt.target.innerHTML = "Light Theme";
+            document.getElementById("toggleColor").classList.remove('btn-dark');
+            document.getElementById("toggleColor").classList.add('btn-light');
+            svgLogo.classList.add("is-hidden");
+            svgLogo1.classList.remove("is-hidden");
 
-        // set currentSvg to first image
-        currentSVG = svgLogo1
+            // set currentSvg to first image
+            currentSVG = svgLogo1
 
-    } else {
-        cardTemplate.style.backgroundColor = "white";
-        cardTemplate.firstElementChild.style.color = "#6c757d ";
-        evt.target.innerHTML = "Dark Theme";
-        document.getElementById("toggleColor").classList.remove('btn-light');
-        document.getElementById("toggleColor").classList.add('btn-dark');
-        svgLogo.classList.remove("is-hidden");
-        svgLogo1.classList.add("is-hidden");
+        } else {
+            cardTemplate.style.backgroundColor = "white";
+            cardTemplate.firstElementChild.style.color = "#6c757d ";
+            evt.target.innerHTML = "Dark Theme";
+            document.getElementById("toggleColor").classList.remove('btn-light');
+            document.getElementById("toggleColor").classList.add('btn-dark');
+            svgLogo.classList.remove("is-hidden");
+            svgLogo1.classList.add("is-hidden");
 
-        // set currentSvg to second image
-		currentSVG = svgLogo
+            // set currentSvg to second image
+            currentSVG = svgLogo
 
-    }
+        }
 
-})
-// Function to download image
+    })
+    // Function to download image
 function triggerDownload(imgURI) {
     var evt = new MouseEvent('click', {
         view: window,
@@ -58,7 +58,7 @@ function triggerDownload(imgURI) {
 }
 
 // Add click event to download button
-document.getElementById('myBtn').addEventListener('click', function () {
+document.getElementById('myBtn').addEventListener('click', function() {
     changeCollegeName();
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
@@ -73,7 +73,7 @@ document.getElementById('myBtn').addEventListener('click', function () {
     });
     var url = DOMURL.createObjectURL(svgBlob);
 
-    img.onload = function () {
+    img.onload = function() {
         ctx.drawImage(img, 0, 0);
         DOMURL.revokeObjectURL(url);
 
@@ -92,9 +92,10 @@ document.getElementById('myBtn').addEventListener('click', function () {
 
 // Dynamic update of college name on keychange 
 var keyChange = document.getElementById('collegeName');
-keyChange.onkeyup = keyChange.onkeypress = function () {
+keyChange.onkeyup = keyChange.onkeypress = function() {
     changeCollegeName();
 }
+
 function changeCollegeName() {
     var collegeName = reverseString(document.getElementById('collegeName').value);
     document.getElementById('logoName1').textContent = collegeName;
